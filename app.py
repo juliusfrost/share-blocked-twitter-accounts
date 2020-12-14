222c3d31f20d77e7ded8ecf2b67cabf039d471c4
Julius Frost <33183774+juliusfrost@users.noreply.github.com>
2020-12-14 01:35:37 -0500

    log token

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def callback():
 
     # unless oauth_token is still stored locally, return error
     if oauth_token not in oauth_store:
-        return render_template('error.html', error_message="oauth_token not found locally")
+        return render_template('error.html', error_message="oauth_token (" + oauth_token + ") not found locally")
 
     oauth_token_secret = oauth_store[oauth_token]
 
